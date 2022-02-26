ffd391e9efb4ddb6a6771547c19ef40e7d134b58
Sacylkowski <sacylkowski@Sarahs-MBP.austin.rr.com>
2022-02-26 09:11:04 -0600

    Added container with buttons and styles in CSS

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Create function
+
+// Event listener for start button
+
+// Once button is clicked, function runs the quiz and setinterval
+
+// Have an object of question array, choices array, answer array
+
+// When correct answer is picked, next question pops up
+
+// Else the answer is incorrect and time gets subtracted from clock (Decrement)
+
+// When all questions have gone through or time is out - the game is over
+
+// When game is over, then I can save initials and the score into local storage
+
+var num1 = 5;
+var num2 = 7;
+
+if (num1 > num2){
+    console.log("num1 is bigger");
+}
